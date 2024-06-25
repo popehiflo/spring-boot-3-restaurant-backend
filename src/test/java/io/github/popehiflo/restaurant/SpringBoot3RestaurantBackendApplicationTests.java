848f99f1c69c07e462e686d46e6bd29a3df503d4
Pool Petter Hijuela Florian <poolpetterhijuel@hotmail.com>
2024-06-25 16:57:54 -0500

    feat: initialize project (backend) with spring initialzr

---
+++ src/test/java/io/github/popehiflo/restaurant/SpringBoot3RestaurantBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.popehiflo.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3RestaurantBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
